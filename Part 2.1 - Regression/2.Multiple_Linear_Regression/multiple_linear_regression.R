98346cf0d26eba31e1294374aff549118f82a986
Hasib Iqbal <pranto00250@gmail.com>
2017-06-02 16:44:09 +0600

    Multiple Linear Regression with r complete

--- Part 2.1 - Regression/2.Multiple_Linear_Regression/multiple_linear_regression.R
+++ Part 2.1 - Regression/2.Multiple_Linear_Regression/multiple_linear_regression.R
@@ -21,8 +21,41 @@ test_set = subset(dataset, split == FALSE)
 # test_set = scale(test_set)
 
 # Fitting Multiple Linear Regression to the Training set
+#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+#               data = training_set) is same as below 
+
 regressor = lm(formula = Profit ~ .,
                data = training_set)
 
+# summary(regressor)
+ 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# Builind the optimal model using Backward Elimination with R
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset) 
+
+summary(regressor)
+
+
+# Will eliminate State as its corresponding p-value is higher than significant level
+
+
+regressor = lm(formula = Profit ~ R.D.Spend   + Marketing.Spend ,
+               data = dataset) 
+
+summary(regressor)
+
+
+# Will eliminate Administration as its corresponding p-value is higher than significant level
+
+regressor = lm(formula = Profit ~ R.D.Spend  ,
+               data = dataset) 
+
+summary(regressor)
+
+
+# Will eliminate Marketing.Spend as its corresponding p-value is higher than significant level
+
