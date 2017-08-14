40a674a92f3af9c3d718d67c156e2f8e400662d8
root <r.haque.249.rh@gmail.com>
2017-08-14 22:08:33 +0600

    Classification

--- Part 3.2 - Classsification/2. K-Nearest Neighbors (K-NN)/knn.py
+++ Part 3.2 - Classsification/2. K-Nearest Neighbors (K-NN)/knn.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 11 22:26:32 2017
+Created on Mon Aug 14 19:57:28 2017
 
 @author: Md. Rezwanul Haque
 """
-# Classification Template
+# K-Nearest Neighbors (K-NN)
 
 # Importing the libraries
 import numpy as np
@@ -28,7 +28,9 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 # Fitting the classifier to the Training set
-# Create your classifier here
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -49,7 +51,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regresssion (Training set)')
+plt.title('K-NN (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -67,10 +69,9 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regresssion (Test set)')
+plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
 
